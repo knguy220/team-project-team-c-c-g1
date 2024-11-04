2a44abf92e33189e4f78d2957f63bab91b910577
Isabella <i_nogues@u.pacific.edu>
2024-11-04 14:23:04 -0800

    class info

--- src/WeaponType.java
+++ src/WeaponType.java
@@ -1,4 +1,16 @@
 
-public class WeaponType {
 
+public enum WeaponType {
+	FIST, GUN, HAZMATSUIT, FLYSWAT, BUGREPELLENT;
+	
+	public String toString() {
+		switch(this) {
+			case FIST: return "fist";
+			case GUN: return "gun";
+			case HAZMATSUIT: return "hazmat suit";
+			case FLYSWAT: return "flyswat";
+			case BUGREPELLENT: return "bug repellent";
+		}
+		return "n/a";
+	}
 }
