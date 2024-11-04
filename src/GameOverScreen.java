f412c62d3fb61752fd0c59654d3317e45bc5bd9d
Gianna Solano <g_solano1@u.pacific.edu>
2024-11-04 14:16:27 -0800

    mouse events

--- src/GameOverScreen.java
+++ src/GameOverScreen.java
@@ -1,4 +1,16 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class GameOverScreen {
-
+	public void MouseReleased (MouseEvent e) {
+		
+	}
+	public void MouseClicked (MouseEvent e) {
+		
+	}
+	public void MouseMoved (MouseEvent e) {
+		
+	}
 }
