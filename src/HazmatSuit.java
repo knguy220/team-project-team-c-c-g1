efd728ec8f08c7308393b7294b35675c34af0daf
Isabella <i_nogues@u.pacific.edu>
2024-11-05 00:16:11 -0800

    hazmat suit class set up

---
+++ src/HazmatSuit.java
@@ -0,0 +1,11 @@
+
+public class HazmatSuit extends Weapon{
+
+	public HazmatSuit(WeaponType type, int damgeCaused) {
+		super(type, damgeCaused);
+	}
+	public void Activate(){
+		//TODO what happens when activated 
+	}
+
+}
