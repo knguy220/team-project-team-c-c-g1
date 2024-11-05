b5ea266a94422deb88c74f26edb264f891150334
Isabella <i_nogues@u.pacific.edu>
2024-11-05 00:21:59 -0800

    bug repellent set up

---
+++ src/BugRepellent.java
@@ -0,0 +1,12 @@
+
+public class BugRepellent extends Weapon {
+
+	public BugRepellent(WeaponType type, int damgeCaused) {
+		super(type, damgeCaused);
+	}
+	
+	public void Activate(){
+		//TODO what happens when activated
+	}
+
+}
