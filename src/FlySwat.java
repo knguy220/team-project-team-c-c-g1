21771bed27ea22a6b8f66f7ca9d5089b27e83008
Isabella <i_nogues@u.pacific.edu>
2024-11-05 00:18:41 -0800

    flysawt class set up

---
+++ src/FlySwat.java
@@ -0,0 +1,11 @@
+
+public class FlySwat extends Weapon{
+
+	public FlySwat(WeaponType type, int damgeCaused) {
+		super(type, damgeCaused);
+	}
+	public void Activate(){
+		//TODO what happens when activated 
+	}
+
+}
