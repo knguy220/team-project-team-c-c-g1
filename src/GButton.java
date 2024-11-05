a0092f38daecd32e623a9802afd46f24e1565e04
ZachJG <z_goloyogo@u.pacific.edu>
2024-11-04 21:29:19 -0800

    GButton

--- src/GButton.java
+++ src/GButton.java
@@ -2,38 +2,59 @@ import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class GButton extends GraphicsProgram {
+	private static int PROGRAM_WIDTH = 800;
+	private static int PROGRAM_HEIGHT = 600;
 	private GRect rect;
 	private GLabel message;
 	
-	GButton(GRect r, GLabel m) {
-		rect = r;
-		message = m;
+	GButton(double x, double y, double w, double h, String t, String f, Color r, Color l) {
+		rect = new GRect(x,y,w,h);
+		rect.setColor(r);
+		rect.setFilled(true);
+		message = new GLabel(t,x+(rect.getWidth()/4),y+(rect.getHeight()/2));
+		message.setFont(f);
+		message.scale(w/100,h/50);
+		message.setColor(l);
 	}
-	public void sizeLabel(double w, double h) {
-		message.scale(w, h);
-	}
-	public void setRectColor(Color e) {
-		rect.setFillColor(e);
-	}
-	public void setLabelMessage(String s) {
+	public void setLabelMessage(String s, String f) {
+		message.setFont(f);;
 		message.setLabel(s);
 	}
-	public void setLabelColor(Color e) {
-		message.setColor(e);
+	public void setUp() {
+		add(rect);
+		add(message);
 	}
-	public void setLabelFont(Font f) {
-		message.setFont(f);
+	public void setButtonLocation(double x, double y) {
+		rect.setLocation(x,y);
+		message.setLocation(x+(rect.getWidth()/4), y+(rect.getHeight()/2));
+	}
+	public void setButtonSize(double w, double h) {
+		rect.setSize(w, h);
+		message.scale(w/100, h/50);
+	}
+	public void setButtonColor(Color e, Color t) {
+		rect.setColor(e);
+		rect.setFilled(true);
+		message.setColor(t);
+	}
+	
+	public void init() {
+		setSize(PROGRAM_WIDTH,PROGRAM_HEIGHT);
+		requestFocus();
 	}
 	
 	public static void main(String[] args) {
-		
+		GButton button = new GButton(200,200,100,50,"BUTTON","SANS_SERIF",Color.black,Color.green);
+		button.start();
 	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		setUp();
+		addMouseListeners();
 	}
 } 
