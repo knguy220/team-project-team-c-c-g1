bf491571a2cae05675eeb3206e5298650f49276d
Gianna Solano <g_solano1@u.pacific.edu>
2024-11-06 13:45:34 -0800

    setting up screen

--- src/GameOverScreen.java
+++ src/GameOverScreen.java
@@ -3,13 +3,18 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 
 public class GameOverScreen extends GraphicsProgram implements ActionListener {
 	private GLabel title;
 	private GLabel score;
+	private GLabel again;
+	private GRect restart;
+	private GImage icon;
 	
 	public static final int WINDOW_HEIGHT = 600;
 	public static final int WINDOW_WIDTH = 600; 
@@ -17,14 +22,41 @@ public class GameOverScreen extends GraphicsProgram implements ActionListener {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		title = new GLabel ("Game Over!", WINDOW_WIDTH/3, WINDOW_HEIGHT/6);
+		title = new GLabel ("Game Over!");
 		title.setColor(Color.black);
 		title.setFont("Arial-BOLD-36");
+		double titleX = (WINDOW_WIDTH - title.getWidth()) / 2;
+		double titleY = WINDOW_HEIGHT / 6; 
+		title.setLocation(titleX, titleY);
 		add(title);
+		    
 		score = new GLabel (/*userName +*/ "-----'s score: " + /*userScore + */ "---- points", WINDOW_HEIGHT/3, WINDOW_WIDTH/2);
 		score.setColor(Color.black);
 		score.setFont("Arial-BOLD-24");
-		add(score);
+		double scoreX = (WINDOW_WIDTH - score.getWidth()) / 2; 
+	    double scoreY = WINDOW_HEIGHT / 2; 
+	    score.setLocation(scoreX, scoreY);
+	    add(score);
+	    
+	    restart = new GRect(75, 75);
+	    double restartX = (WINDOW_WIDTH - restart.getWidth()) / 2;
+	    double restartY = (WINDOW_HEIGHT * 0.75) - (restart.getHeight() / 2); 
+	    restart.setLocation(restartX, restartY);
+	    add(restart);
+
+	    
+	    again = new GLabel("Play Again?");
+	    again.setColor(Color.black);
+	    again.setFont("Arial-BOLD-30");
+	    double againX = (WINDOW_WIDTH - again.getWidth()) / 2; 
+	    double againY = restartY - 10; 
+	    again.setLocation(againX, againY);
+	    add(again);
+	    
+	   
+	    /*icon = new GImage("arrow.png");
+	    icon.setLocation(againX, againY);
+	    add(icon);*/
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -43,6 +75,7 @@ public class GameOverScreen extends GraphicsProgram implements ActionListener {
 	}
 	public void init() {
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		requestFocus();
 	}
 	public static void main(String args[]) {
 		new GameOverScreen().start();
