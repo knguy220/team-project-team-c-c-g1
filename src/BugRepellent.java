3d8919195d4a71f2a02146e684881cce200eeb37
Isabella <i_nogues@u.pacific.edu>
2024-11-06 14:57:38 -0800

    bug repellent cooldown

--- src/BugRepellent.java
+++ src/BugRepellent.java
@@ -1,14 +1,35 @@
+import java.util.concurrent.TimeUnit;
 
 public class BugRepellent extends Weapon {
-	private int coolDownTime;
+	private int coolDownTime = 120; //2 min
 	private int enemiesKilled;
+	private boolean isOnCoolDown = false;
 
 	public BugRepellent(WeaponType type, int damgeCaused) {
 		super(type, damgeCaused);
 	}
 	
 	public void Activate(){
-		//TODO what happens when activated
+		if(isOnCoolDown){
+			System.out.println("Bug Repellent Cooling Down");
+			return;
+		}
+		System.out.println("Bug Repellent Suit Activated");
+		//TODO what happens when activated 
+		
+		startCoolDown();
+	}
+	private void startCoolDown(){
+		isOnCoolDown = true;
+		new Thread(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(coolDownTime);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			isOnCoolDown = false;
+			// ready for activation
+		}).start();
 	}
 
 }
