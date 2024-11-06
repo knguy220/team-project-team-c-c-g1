b25ad3af294908cf7bf92fb97a26ac36824e6b29
ZachJG <z_goloyogo@u.pacific.edu>
2024-11-05 19:24:43 -0800

    GButton and Instructions

--- src/GButton.java
+++ src/GButton.java
@@ -2,57 +2,23 @@ import acm.graphics.*;
 import acm.program.*;
 import acm.util.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
 
-public class GButton extends GraphicsProgram {
-	private static int PROGRAM_WIDTH = 800;
-	private static int PROGRAM_HEIGHT = 600;
+
+public class GButton {
 	private GRect rect;
 	private GLabel message;
-	
-	GButton(double x, double y, double w, double h, String t, String f, Color r, Color l) {
+	public GButton(String s, String f, double x, double y, double w, double h, Color r, Color l) {
 		rect = new GRect(x,y,w,h);
 		rect.setColor(r);
 		rect.setFilled(true);
-		message = new GLabel(t,x+(rect.getWidth()/4),y+(rect.getHeight()/2));
+		message = new GLabel(s,x+rect.getWidth()/4,y+rect.getHeight()/2);
 		message.setFont(f);
 		message.setColor(l);
 	}
-	public void setLabelMessage(String s, String f) {
-		message.setFont(f);;
-		message.setLabel(s);
+	public GLabel getMessage() {
+		return message;
 	}
-	public void setUp() {
-		add(rect);
-		add(message);
+	public GRect getRect() {
+		return rect;
 	}
-	public void setButtonLocation(double x, double y) {
-		rect.setLocation(x,y);
-		message.setLocation(x+(rect.getWidth()/4), y+(rect.getHeight()/2));
-	}
-	public void setButtonSize(double w, double h) {
-		rect.setSize(w, h);
-	}
-	public void setButtonColor(Color e, Color t) {
-		rect.setColor(e);
-		rect.setFilled(true);
-		message.setColor(t);
-	}
-	
-	public void init() {
-		setSize(PROGRAM_WIDTH,PROGRAM_HEIGHT);
-		requestFocus();
-	}
-	
-	public static void main(String[] args) {
-		GButton button = new GButton(200,200,100,50,"BUTTON","SANS_SERIF-12",Color.black,Color.green);
-		button.start();
-	}
-	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		setUp();
-		addMouseListeners();
-	}
-} 
+}
--- src/InstructionsPane.java
+++ src/InstructionsPane.java
@@ -15,6 +15,7 @@ public class InstructionsPane extends GraphicsProgram implements ActionListener 
 	private GLabel moving;
 	private GLabel attacking;
 	private GLabel selecting;
+	private GLabel select;
 	private GLabel pickingUp;
 	private GButton button;
 	
@@ -22,6 +23,37 @@ public class InstructionsPane extends GraphicsProgram implements ActionListener 
 		setSize(PROGRAM_WIDTH,PROGRAM_HEIGHT);
 	}
 	public void setUp() {
+		rect = new GRect(PROGRAM_WIDTH/10,PROGRAM_HEIGHT/10,450,200);
+		rect.setColor(Color.gray);
+		rect.setFilled(true);
+		add(rect);
+		goal = new GLabel("The GOAL is to stay alive as long as possible and protect your ship.", PROGRAM_WIDTH/9,PROGRAM_HEIGHT/8);
+		goal.setFont("SERIF-BOLD-15");
+		goal.setColor(Color.blue);
+		add(goal);
+		moving = new GLabel("To move, use the W, A, S, D keys.", PROGRAM_WIDTH/6,PROGRAM_HEIGHT/6);
+		moving.setFont("SERIF-13");
+		moving.setColor(Color.blue);
+		add(moving);
+		attacking = new GLabel("Use your mouse to fire and aim.", PROGRAM_WIDTH/6,moving.getY()+24);
+		attacking.setFont("SERIF-13");
+		attacking.setColor(Color.blue);
+		add(attacking);
+		selecting = new GLabel("Key 1 selects your gun and Key 2 selects your melee.", PROGRAM_WIDTH/6,moving.getY()+48);
+		selecting.setFont("SERIF-13");
+		selecting.setColor(Color.blue);
+		add(selecting);
+		select = new GLabel("Keys 3, 4, and 5 select your power-ups.",PROGRAM_WIDTH/6,moving.getY()+60);
+		select.setFont("SERIF-13");
+		select.setColor(Color.blue);
+		add(select);
+		pickingUp = new GLabel("Use E to pick up health and ammo.", PROGRAM_WIDTH/6,select.getY()+24);
+		pickingUp.setFont("SERIF-13");
+		pickingUp.setColor(Color.blue);
+		add(pickingUp);
+		button = new GButton("BACK","ARIAL-18",rect.getWidth()/2,rect.getHeight(),100,50,Color.black,Color.blue);
+		add(button.getRect());
+		add(button.getMessage());
 		
 	}
 	
@@ -29,7 +61,15 @@ public class InstructionsPane extends GraphicsProgram implements ActionListener 
 		
 	}
 	public void MouseClicked(MouseEvent e) {
-		
+		rect.setVisible(false);
+		goal.setVisible(false);
+		moving.setVisible(false);
+		attacking.setVisible(false);
+		selecting.setVisible(false);
+		select.setVisible(false);
+		pickingUp.setVisible(false);
+		button.getRect().setVisible(false);
+		button.getMessage().setVisible(false);
 	}
 	public void MouseMoved(MouseEvent e) {
 		
@@ -37,11 +77,12 @@ public class InstructionsPane extends GraphicsProgram implements ActionListener 
 	
 	
 	public static void main(String[] args) {
-		
+		new InstructionsPane().start();
 	}
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		setUp();
+		addMouseListeners();
 	}
 }
