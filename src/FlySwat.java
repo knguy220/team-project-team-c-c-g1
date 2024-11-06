8c8555d09cd3723213924d778fde51b26553e989
Isabella <i_nogues@u.pacific.edu>
2024-11-06 14:53:27 -0800

    flyswat cool down
    Merge branch 'main' of
    https://github.com/Fall2024Comp55/team-project-team-c-c-g.git into main

--- src/FlySwat.java
+++ src/FlySwat.java
@@ -1,13 +1,34 @@
+import java.util.concurrent.TimeUnit;
 
 public class FlySwat extends Weapon{
-	private int coolDownTime; 
+	private int coolDownTime = 60; // 1 min
 	private int enemiesKilled;
+	private boolean isOnCoolDown = false; 
 
 	public FlySwat(WeaponType type, int damgeCaused) {
 		super(type, damgeCaused);
 	}
 	public void Activate(){
+		if(isOnCoolDown){
+			System.out.println("Fly swat is cooling down");
+			return;
+		}
+		System.out.println("Fly sawt is activated");
 		//TODO what happens when activated 
+		startCoolDown();
+	}
+	
+	private void startCoolDown(){
+		isOnCoolDown = true;
+		new Thread(() -> {
+			try {
+				TimeUnit.SECONDS.sleep(coolDownTime);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			isOnCoolDown = false;
+			// ready for activation
+		}).start();
 	}
 
 }
