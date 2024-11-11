4d027eaf0c82816bece1ff0ad543e3650fe9d146
Isabella <i_nogues@u.pacific.edu>
2024-11-11 14:54:23 -0800

    enemies threshold

--- src/Weapon.java
+++ src/Weapon.java
@@ -2,6 +2,10 @@
 public class Weapon {
 	private WeaponType type;
 	private int damageCaused; 
+	private int enemiesKilled = 0;
+	private HazmatSuit hazmat; 
+	private FlySwat flySwat;
+	private BugRepellent BR;
 
 
 public Weapon (WeaponType type, int damageCaused){
@@ -21,7 +25,18 @@ public void setDamage(int damageCaused){
 	this.damageCaused = damageCaused; 
 }
 
-
+public void isEnemyKilled(){
+	enemiesKilled++;
+	if(enemiesKilled == 10){
+		hazmat.Activate();
+	}
+	if(enemiesKilled == 25){
+		flySwat.Activate();
+	}
+	if(enemiesKilled == 35){
+		BR.Activate();
+	}
+}
 
 }
 
