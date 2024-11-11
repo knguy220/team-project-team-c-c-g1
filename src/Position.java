9c4a6b666e1059b9f8f079cadd5d4bcc65fd6115
Gianna Solano <g_solano1@u.pacific.edu>
2024-11-11 14:25:31 -0800

    position class and updated game over

--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,33 @@
 
-public class Position {
 
-}
+	public class Position {
+		private int posX;
+		private int posY;
+		
+		public Position(int posX, int posY) {
+			this.posX = posX;
+			this.posY = posY;
+			
+		}
+		
+		public int getX() {
+			
+			return posX;
+		}
+		public void setX(int posX) {
+			this.posX = posX;
+		}
+		public int getY() {
+		
+			return posY;
+		}
+		public void setY(int posY) {
+			this.posY = posY;
+		}
+		public static void main(String[] args) {
+			
+		}
+	}
+
+
+
