787c941c0d6e90e15114ff52136a781657feb6d4
Isabella <i_nogues@u.pacific.edu>
2024-11-13 14:30:11 -0800

    remaining private variables

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,7 +1,11 @@
+import java.awt.Color;
+import acm.graphics.GImage;
 
 public class Enemy {
 	private int Health;
 	private int Damage;
 	private int speed;
-	
+	private Position position; 
+	private GImage bugIcon;
+	private Color bugColor;
 }
