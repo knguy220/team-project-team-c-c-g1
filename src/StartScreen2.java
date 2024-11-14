66608c03ca9e32da52d79728fb3730a72710595a
Kennedy Nguyen <k_nguyen198@u.pacific.edu>
2024-11-14 07:39:09 -0800

    added photos and adjusted startScreen2

--- src/StartScreen2.java
+++ src/StartScreen2.java
@@ -11,6 +11,7 @@ public class StartScreen2 {
     private GButton settingsButton;
     private GButton instructionsButton;
     private GImage backgroundImage;
+    private GImage gameTitleImage;
 
 
     public StartScreen2(GameApp gameApp) {
@@ -20,15 +21,13 @@ public class StartScreen2 {
     public void show() {
         gameApp.removeAll();
         
-        backgroundImage = new GImage("universe.jpg");
+        backgroundImage = new GImage("clouds.png");
         backgroundImage.setSize(gameApp.getWidth(), gameApp.getHeight());
         gameApp.add(backgroundImage);
-
-        // Title label for the game
-        GLabel titleLabel = new GLabel("Commander Dez", 585, 100);
-        titleLabel.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 60));
-        titleLabel.setColor(Color.WHITE);
-        gameApp.add(titleLabel);
+        
+        gameTitleImage = new GImage("commanderfont.png");
+        gameTitleImage.setLocation((gameApp.getWidth() - gameTitleImage.getWidth()) / 2, 50);
+        gameApp.add(gameTitleImage);
 
         // Settings button - Opens the settings screen when clicked
         settingsButton = new GButton("Settings", 730, 240, 100, 40, Color.BLACK, Color.WHITE);
@@ -89,7 +88,7 @@ public class StartScreen2 {
             "Survive as long as possible. Use W, A, S, D to move. " +
             "Use the mouse to aim and shoot. Use E to pick up items.", 400, 385);
         instructionsLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-        instructionsLabel.setColor(Color.BLUE);
+        instructionsLabel.setColor(Color.WHITE);
         gameApp.add(instructionsLabel);
     }
 }
