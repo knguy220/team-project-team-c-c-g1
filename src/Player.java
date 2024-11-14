23e77674a674b2c569851da7a9a858e612ccc05e
ZachJG <z_goloyogo@u.pacific.edu>
2024-11-13 20:03:45 -0800

    Added set and get functions in Player class.

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,30 @@
+import acm.graphics.*;
+import java.awt.*;
+
 
 public class Player {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int health;
+	private int damage;
+	private int speed;
+	private Position position;
+	private GImage playerBody;
+	
+	public void setSpeed(int s) {
+		speed = s;
 	}
-
+	public int getSpeed() {
+		return speed;
+	}
+	public void setHealth(int h) {
+		health = h;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setDamage(int d) {
+		damage = d;
+	}
+	public int getDamage() {
+		return damage;
+	}
 }
