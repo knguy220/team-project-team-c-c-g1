7407bb6b1ae16279080979c4f15d4831cae4a3d7
ZachJG <z_goloyogo@u.pacific.edu>
2024-11-14 21:54:13 -0800

    Added functions that change the GImage of Player.

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,25 @@
+import javax.swing.Timer;
 import acm.graphics.*;
-import java.awt.*;
+import acm.program.*;
+import java.awt.event.*;
 
 
-public class Player {
+
+public class Player extends GraphicsProgram implements ActionListener {
 	private int health;
 	private int damage;
 	private int speed;
 	private Position position;
 	private GImage playerBody;
+	private boolean isMoving;
+	private double playerScale;
+	private String walk1 = "Walk1.png";
+	private String walk2 = "Walk2.png";
+	private String stand = "Stand.png";
+	private int foot;
+	private double playerX;
+	private double playerY;
+	
 	
 	public void setSpeed(int s) {
 		speed = s;
@@ -26,5 +38,64 @@ public class Player {
 	}
 	public int getDamage() {
 		return damage;
+	}
+	public void setImage(String name, double x, double y, double s) {
+		playerBody = new GImage(name,x,y);
+		playerX = x;
+		playerY = y;
+		playerBody.scale(s);
+		playerScale = s;
+	}
+	public GImage getImage() {
+		return playerBody;
+	}
+	public void setMove(boolean m) {
+		isMoving = m;
+	}
+	public boolean getMove() {
+		return isMoving;
+	}
+	public void setFoot(int f) {
+		foot = f;
+	}
+	public int getFoot() {
+		return foot;
+	}
+	public void increaseFoot() {
+		foot++;
+		if (foot >= 2) {
+			setFoot(0);
+		}
+	}
+	public void changeImage() {
+		if (getFoot() == -1) {
+			setImage(stand,playerX,playerY,playerScale);
+		} else if (getFoot() == 0) {
+			setImage(walk1,playerX,playerY,playerScale);
+		} else if (getFoot() == 1) {
+			setImage(walk2,playerX,playerY,playerScale);
+		}
+	}
+	
+	public void init() {
+		setSize(800,800);
+		setImage(stand,200,200,0.5);
+		setMove(true);
+		setFoot(1);
+		changeImage();
+		add(getImage());
+	}
+	
+	public static void main(String[] args) {
+		new Player().start();
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 }
