6a0723b5b33af9f836bfda53be862a634fd6c0ec
Kennedy Nguyen <k_nguyen198@u.pacific.edu>
2024-11-16 16:26:17 -0800

    implemented bullet class, adjusted GameApp, and StartGame

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,38 +1,77 @@
-import acm.graphics.GImage;
+import acm.graphics.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
 
 public class Bullet {
-	private GImage bulletIcon;
-	private Position bulletPosition; 
-	private Position clickedLocation;
-	
-	public Bullet (GImage bulletIcon, Position startPosition, Position targetPosition) {
-		this.bulletIcon = bulletIcon;
-		this.bulletPosition = startPosition;
-		this.clickedLocation = targetPosition;
-		bulletIcon = new GImage ("rename.png");
-		
-	}
-	public void bulletMovementPath(int X, int Y) {
-		double differenceX = clickedLocation.getX() - bulletPosition.getX();
-		double differenceY = clickedLocation.getY() - bulletPosition.getY();
-		
-		//double distance = Math.sqrt(differenceX * differenceX + differenceY * differenceY);
-		
-		double fraction = 0.5;
-		
-		int moveX = (int) (differenceX * fraction);
-		int moveY = (int) (differenceY * fraction);
-		
-		bulletPosition.setX(bulletPosition.getX() + moveX);
-	    bulletPosition.setY(bulletPosition.getY() + moveY);
+    private static final int BULLET_SIZE = 5;
+    private static final int BULLET_SPEED = 15;
+    private static final int BULLET_LIFETIME = 5000; 
+    private static final int SHOOT_COOLDOWN = 500; 
+    private GOval bulletShape;
+    private double directionX;
+    private double directionY;
+    private Timer bulletTimer;
+    private GameApp gameApp;
+    private StartGame startGame;
+    private static long lastShootTime = 0;
 
-	     
-	    bulletIcon.setLocation(bulletPosition.getX(), bulletPosition.getY());
-	}
-	
-	public void targetLocation(Position newTarget) {
-		this.clickedLocation = newTarget;
-	}
-	
-	
+    public Bullet(GameApp gameApp, StartGame startGame, double startX, double startY, double directionX, double directionY) {
+        this.gameApp = gameApp;
+        this.startGame = startGame;
+        this.directionX = directionX;
+        this.directionY = directionY;
+
+        bulletShape = new GOval(startX, startY, BULLET_SIZE, BULLET_SIZE);
+        bulletShape.setFilled(true);
+        bulletShape.setColor(Color.RED);
+        gameApp.add(bulletShape);
+
+        // Timer to control bullet movement
+        bulletTimer = new Timer(30, this::moveBullet); 
+        bulletTimer.start();
+    }
+
+    public static boolean canShoot() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastShootTime >= SHOOT_COOLDOWN) {
+            lastShootTime = currentTime;
+            return true;
+        }
+        return false;
+    }
+
+    public void reinitialize(double startX, double startY, double directionX, double directionY) {
+        this.directionX = directionX;
+        this.directionY = directionY;
+        bulletShape.setLocation(startX, startY);
+        bulletTimer.restart();
+    }
+
+    private void moveBullet(ActionEvent e) {
+        bulletShape.move(BULLET_SPEED * directionX, BULLET_SPEED * directionY);
+        if (isOutOfBounds()) {
+            destroyBullet();
+        }
+    }
+
+    private boolean isOutOfBounds() {
+        double x = bulletShape.getX();
+        double y = bulletShape.getY();
+        double screenBuffer = 10; 
+        return x < -screenBuffer || x > gameApp.getWidth() + screenBuffer || y < -screenBuffer || y > gameApp.getHeight() + screenBuffer;
+    }
+
+    private void destroyBullet() {
+        if (bulletTimer != null) {
+            bulletTimer.stop();
+        }
+        gameApp.remove(bulletShape);
+        startGame.removeBullet(this);
+    }
+
+    public GOval getBulletShape() {
+        return bulletShape;
+    }
 }
+
--- src/GameApp.java
+++ src/GameApp.java
@@ -1,143 +1,4 @@
-//import acm.program.GraphicsProgram; 
-//import acm.graphics.*;
-//import java.awt.Toolkit;
-//import java.awt.Dimension;
-//import java.awt.event.KeyEvent;
-//import java.awt.event.MouseEvent;
-//
-//public class GameApp extends GraphicsProgram {
-//    private StartScreen2 startScreen;
-//    private SettingsScreen settingsScreen;
-//    private StartGame startGame;
-//    private PauseScreen pauseScreen;
-//    private boolean isPaused = false;
-//    private boolean fromPauseScreen = false;
-//
-//    public void init() {
-//        startScreen = new StartScreen2(this);
-//        settingsScreen = new SettingsScreen(this);
-//        pauseScreen = new PauseScreen(this);
-//
-//        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-//        setSize(screenSize.width, screenSize.height);
-//
-//        showStartScreen();
-//        addMouseListeners();
-//        addKeyListeners();
-//    }
-//
-//    public void showStartScreen() {
-//        if (startGame != null) startGame.hide();
-//        startScreen.show();
-//    }
-//
-//    public void showSettingsScreen(boolean openedFromPause) {
-//        fromPauseScreen = openedFromPause;
-//
-//        if (fromPauseScreen) {
-//            pauseScreen.hide();
-//        } else {
-//            if (startGame != null) startGame.hide();
-//            startScreen.hide();
-//        }
-//
-//        settingsScreen.show();
-//    }
-//
-//    public void startGame() {
-//        removeAll();
-//        if (startGame == null) {
-//            startGame = new StartGame(this);
-//        } else {
-//            startGame.show();
-//        }
-//        isPaused = false;
-//    }
-//
-//    public void showPauseScreen() {
-//        if (startGame != null) startGame.hide();
-//        pauseScreen.show();
-//        isPaused = true;
-//    }
-//
-//    public void resumeGame() {
-//        if (startGame != null) {
-//            pauseScreen.hide();
-//            startGame.show();
-//        }
-//        isPaused = false;
-//    }
-//
-//    public void quitToStartScreen() {
-//        if (startGame != null) startGame.hide();
-//        showStartScreen();
-//    }
-//
-//    @Override
-//    public void keyPressed(KeyEvent e) {
-//        if (startGame != null && !isPaused) {
-//            startGame.handleKeyPress(e);
-//        }
-//    }
-//
-//    @Override
-//    public void mouseMoved(MouseEvent e) {
-//        if (startGame != null && !isPaused) {
-//            startGame.updateAiming(e);
-//        }
-//    }
-//
-//    @Override
-//    public void mousePressed(MouseEvent e) {
-//        if (startGame != null && !isPaused) {
-//            startGame.handleShooting(e);
-//        }
-//    }
-//
-//    @Override public void keyReleased(KeyEvent e) {}
-//    @Override public void keyTyped(KeyEvent e) {}
-//
-//    @Override
-//    public void mouseClicked(MouseEvent e) {
-//        if (isPaused && pauseScreen.isResumeButtonClicked(e.getX(), e.getY())) {
-//            resumeGame();
-//        } else if (isPaused && pauseScreen.isSettingsButtonClicked(e.getX(), e.getY())) {
-//            showSettingsScreen(true);
-//        } else if (isPaused && pauseScreen.isQuitButtonClicked(e.getX(), e.getY())) {
-//            quitToStartScreen();
-//        } else if (settingsScreen.isApplyButtonClicked(e.getX(), e.getY())) {
-//            settingsScreen.applySettings();
-//        } else if (settingsScreen.isBackButtonClicked(e.getX(), e.getY())) {
-//            settingsScreen.hide();
-//            if (fromPauseScreen) {
-//                resumeGame();
-//            } else {
-//                showStartScreen();
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void mouseDragged(MouseEvent e) {
-//        settingsScreen.handleSliderDrag(e);
-//    }
-//
-//    @Override
-//    public void mouseReleased(MouseEvent e) {
-//        settingsScreen.handleMouseReleased();
-//    }
-//
-//    public static void main(String[] args) {
-//        new GameApp().start();
-//    }
-//
-//	@Override
-//	public void run() {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//}
-import acm.program.GraphicsProgram; 
+import acm.program.GraphicsProgram;
 import acm.graphics.*;
 import java.awt.Toolkit;
 import java.awt.Dimension;
@@ -233,9 +94,6 @@ public class GameApp extends GraphicsProgram {
         }
     }
 
-    @Override public void keyReleased(KeyEvent e) {}
-    @Override public void keyTyped(KeyEvent e) {}
-
     @Override
     public void mouseClicked(MouseEvent e) {
         if (isPaused && pauseScreen.isResumeButtonClicked(e.getX(), e.getY())) {
@@ -268,11 +126,12 @@ public class GameApp extends GraphicsProgram {
 
     @Override
     public void run() {
-        // No additional logic needed; WE can leave this method empty or add specific behavior
+        // No additional logic needed; we can leave this method empty or add specific behavior
     }
 
     public static void main(String[] args) {
         new GameApp().start();
     }
 }
+
 
