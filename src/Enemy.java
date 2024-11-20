4fc218402f8a327322f385d3510a1b87549b063e
Isabella <i_nogues@u.pacific.edu>
2024-11-20 09:06:08 -0800

    trying to fix why i'm not able to pull

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,24 +1,77 @@
+import acm.graphics.*;
 import java.awt.Color;
-import acm.graphics.GImage;
 
 public class Enemy {
-	private int Health; // will update kill count 
-	private int Damage;
-	private int speed;
-	private Position position; 
-	private GImage bugIcon;
-	private Color bugColor;
-	
-	public void moveLeft(){}
-	public void moveRight(){}
-	public void moveUp(){}
-	public void moveDown(){}
-	public void spawnIn(){}
-	public void attack(){}
-	public void beenHit(){}
-	
-}
+    private GOval enemyShape;
+    private double speed;
 
-class BugOne extends Enemy{}
-class BugTwo extends Enemy{}
-class BugThree extends Enemy{}
+    public static final int ENEMY_SIZE = 30;
+
+    public Enemy(GameApp gameApp, double startX, double startY) {
+        // Create the enemy shape
+        enemyShape = new GOval(startX, startY, ENEMY_SIZE, ENEMY_SIZE);
+        enemyShape.setFilled(true);
+        enemyShape.setColor(Color.RED);
+
+        // Define default speed for the enemy
+        this.speed = 2.0;
+
+        // Add the enemy to the game
+        gameApp.add(enemyShape);
+    }
+
+    /**
+     * Moves the enemy toward the player's position.
+     *
+     * @param playerX The X-coordinate of the player's center.
+     * @param playerY The Y-coordinate of the player's center.
+     */
+    public void moveTowardsPlayer(double playerX, double playerY) {
+        double enemyCenterX = enemyShape.getX() + ENEMY_SIZE / 2;
+        double enemyCenterY = enemyShape.getY() + ENEMY_SIZE / 2;
+
+        // Calculate direction vector
+        double directionX = playerX - enemyCenterX;
+        double directionY = playerY - enemyCenterY;
+
+        // Normalize the direction vector
+        double length = Math.sqrt(directionX * directionX + directionY * directionY);
+        if (length != 0) {
+            directionX /= length;
+            directionY /= length;
+        }
+
+        // Move the enemy in the direction of the player
+        enemyShape.move(directionX * speed, directionY * speed);
+    }
+
+    /**
+     * Sets the speed of the enemy.
+     *
+     * @param speed The new speed value for the enemy.
+     */
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * Returns the enemy's shape for collision detection.
+     */
+    public GOval getEnemyShape() {
+        return enemyShape;
+    }
+
+    /**
+     * Returns the size of the enemy for collision detection.
+     */
+    public static int getEnemySize() {
+        return ENEMY_SIZE;
+    }
+
+    /**
+     * Removes the enemy from the screen.
+     */
+    public void removeFromGame(GameApp gameApp) {
+        gameApp.remove(enemyShape);
+    }
+}
